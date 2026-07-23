9e486e85b9a4d13c0a6f862ee6681dfa4eac24f7
rashil sayami <sayamirashil@gmail.com>
2026-07-24 01:14:37 +0545

    change code 1

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	clientIP := r.RemoteAddr
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Hello World from %s\n", clientIP)
+	fmt.Fprintf(w, "this is spartra %s\n", clientIP)
 }
 
 func main() {
